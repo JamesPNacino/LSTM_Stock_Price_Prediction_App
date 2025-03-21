739a4f6fca9844fa9ad0e316818706fea39dacbb
James Nacino <jamespnacino@gmail.com>
2025-03-21 15:07:59 -0400

    Add files via upload

--- web_app/myproject/view_networks/forms.py
+++ web_app/myproject/view_networks/forms.py
@@ -6,7 +6,7 @@ class RunTickerModel_Form(FlaskForm):
 	ticker_query = StringField('Input stock ticker:')
 	pattern = SelectField('Input candlestick pattern:', choices=[('Random', 'Random'), ('Hammer', 'Hammer'), ('Inverted Hammer', 'Inverted Hammer')])
 	days_out = SelectField('Input days out parameter:', choices=[('1', '1'), ('3', '3'), ('5', '5'), ('10', '10'), ('15', '15')], default='10')
-	pct_increase = SelectField('Input percent increase parameter:', choices=[('1.0', '1.0'), ('1.01', '1.01'), ('1.02', '1.02')], default='1.01')
+	pct_increase = SelectField('Input percent increase parameter:', choices=[('1.0', '1.0'), ('1.01', '1.01'), ('1.02', '1.02'), ('1.03', '1.03')], default='1.01')
 	data_end_date = StringField('Ending date for gathering data - must format as yyyy-mm-dd (i.e. 2020-01-23):')
 	submit = SubmitField('Submit')
 
